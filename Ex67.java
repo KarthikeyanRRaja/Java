6dd2424309da348bd08934a0ac44a0d90044c39c
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-07-24 23:20:52 +0530

    Add files via upload

---
+++ Ex67.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex67;
+
+/**
+ *
+ * @author Karthikeyan
+ */
+public class Ex67 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String main_string = "Dream V2";
+   String word = "Crushers";
+   System.out.println(main_string.substring(0, 7) + word + main_string.substring(6));
+    }
+    
+}
