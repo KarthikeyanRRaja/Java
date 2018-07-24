b10b02635bbc88924848e7c32669313aaf178782
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-07-24 23:48:30 +0530

    Add files via upload

---
+++ Ex117.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex117;
+
+/**
+ *
+ * @author Karthikeyan
+ */
+public class Ex117 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n=4;
+        System.out.println("Square Root of"+n+"="+Math.sqrt(n));
+    }
+    
+}
