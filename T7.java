56c1e09c262d097c59adbb2c759c875e6d0dbf61
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-07-25 23:58:43 +0530

    Add files via upload

---
+++ T7.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t7;
+import java.util.*;
+/**
+ *
+ * @author Karthikeyan
+ */
+public class T7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in); 
+
+		float timeSeconds;
+		float mps,kph, mph;
+
+		System.out.print("Input distance in meters: ");
+		float distance = scanner.nextFloat();
+
+		System.out.print("Input hour: ");
+		float hr = scanner.nextFloat();
+
+		System.out.print("Input minutes: ");
+		float min = scanner.nextFloat();
+
+		System.out.print("Input seconds: ");
+		float sec = scanner.nextFloat();
+
+		timeSeconds = (hr*3600) + (min*60) + sec;
+		mps = distance / timeSeconds;
+		kph = ( distance/1000.0f ) / ( timeSeconds/3600.0f );
+		mph = kph / 1.609f;
+
+		System.out.println("Your speed in meters/second is "+mps);
+		System.out.println("Your speed in km/h is "+kph);
+		System.out.println("Your speed in miles/h is "+mph);
+		
+
+		scanner.close();
+    }
+    
+}
