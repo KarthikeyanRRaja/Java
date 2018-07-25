441632a62cb51297e83871c3815b364f6b42c273
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-07-25 23:16:50 +0530

    Add files via upload

---
+++ Ex105.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex105;
+import java.util.*;
+/**
+ *
+ * @author Karthikeyan
+ */
+public class Ex105 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] array_nums = {11, 15, 13, 10, 45, 20, 11, 15};
+    System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+	
+	int result = 0;
+	int l = 2;
+    int start = 0;
+	int end = array_nums.length-l;
+	for(; l > 0; l--)
+	{
+		if(array_nums[start] != array_nums[end])
+			result = 1;
+		else
+		{
+			start++;
+			end++;
+		}
+	}
+	                
+    if (result==1)
+	 {
+	  System.out.printf(String.valueOf(false));
+	 }
+	else
+	 {
+	   System.out.printf(String.valueOf(true));
+	 }
+    }
+    
+}
