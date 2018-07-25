8fd2c605c61fc8ed63c011adfdc45ea97ffca73d
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-07-25 23:18:40 +0530

    Add files via upload

---
+++ Ex106.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex106;
+import java.util.*;
+/**
+ *
+ * @author Karthikeyan
+ */
+public class Ex106 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] array_nums = {11, 15, 13, 10, 45, 20};
+    System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+	
+	if(array_nums.length >1)
+    {          
+    int first = array_nums[0];
+              
+    for(int i = 1; i < array_nums.length; i++)
+        array_nums[i - 1] = array_nums[i];
+                        
+    array_nums[array_nums.length - 1] = first;
+	System.out.println("New Array: "+Arrays.toString(array_nums)); 
+    }
+    }
+}
