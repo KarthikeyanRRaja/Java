d3bcc089be5287e11cb3d15ca42e0e00cc7b7966
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-07-25 22:24:01 +0530

    Add files via upload

---
+++ Ex75.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex75;
+
+/**
+ *
+ * @author Karthikeyan
+ */
+public class Ex75 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] num_array = {50, -20, 0, 30, 40, 60, 10};
+    
+	System.out.println (num_array.length >= 1 && num_array[0] ==  num_array[num_array.length-1]);
+    }
+    
+}
