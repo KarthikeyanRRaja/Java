0aeb1e75ef49cba16b9f8e450bf67c2d4d27f749
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-07-25 22:25:00 +0530

    Add files via upload

---
+++ Ex71.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex71;
+import java.util.*;
+import java.lang.*;
+/**
+ *
+ * @author Karthikeyan
+ */
+public class Ex71 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     String str1 = "Python";    
+	    String str2 = "Tutorial"; 
+	    
+	    System.out.println(str1.substring(1) + str2.substring(1));
+	        
+    }
+    
+}
