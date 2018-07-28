fc1021d0f269540f121eac2a1e1bb1be5cee518f
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-07-28 14:02:49 +0530

    Add files via upload

---
+++ area1.java
@@ -0,0 +1,13 @@
+package area;
+
+public class area1 {
+	public void square(int a)
+	{
+		System.out.println("Srea of Square="+(a*a));
+	}
+public void rect(int l,int b)
+{
+	System.out.println("Area of rectangle="+(l*b));
+
+}
+}
