13ffd4788469197e046780050de5eed416330740
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-08-07 22:34:45 +0530

    Add files via upload

---
+++ lab_53.java
@@ -0,0 +1,17 @@
+package pack1;
+class AB
+{
+	protected void run()
+	{
+		System.out.println("Hello");
+	}
+}
+public class lab_53 extends AB{
+	public static void main(String h[])
+	{
+		lab_53 l=new lab_53();
+		l.run();
+	}
+	
+
+}
