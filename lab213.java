b657260d8a9c1bfa9c443a08c4d32d79ef4c5a62
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-09-13 19:02:48 +0530

    Add files via upload

---
+++ lab213.java
@@ -0,0 +1,12 @@
+	import java.util.*;  
+	class lab213{  
+	 public static void main(String args[]){  
+	  ArrayList<String> al=new ArrayList<String>();  
+	  al.add("Karthi");  
+	  al.add("Dinesh");  
+	  al.add("Ravi");  
+	  al.add("Rock");  
+	  for(String obj:al)  
+	    System.out.println(obj);  
+	 }  
+	}  
